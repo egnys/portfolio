58d86f05abe9d6299e107d81f83674b91c2801bd
Kin-Laptop <flinqwe@gmail.com>
2023-04-04 12:06:07 +0300

    responsive fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ export const Navbar = ({changeLang, t}) => {
           </svg>
         </button>
 
-        <div className="hidden w-full md:block md:w-auto ml-36" id="navbar-default">
+        <div className="hidden w-full md:block md:w-auto ml-36 max-md:m-0" id="navbar-default">
           <ul className="max-md:h-screen font-medium flex items-center flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-navbar dark:border-gray-700 ">
               <li onClick={() => toggle('navbar-default')}>
               <Link to='/' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{t("Home")}</Link>
